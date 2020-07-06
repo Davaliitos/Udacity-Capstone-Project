0c472f60866967df7bba45760f65fb966534cf4c
Fernando Dávalos <fernando.davalos@freudenberg-it.com>
2020-07-06 14:16:56 -0500

    feat: Added react client

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '1x47agmotc'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'bassethound.us.auth0.com',            // Auth0 domain
+  clientId: 'qdQKHJbumjydgDtABMZLNMOGZPQDf9MQ',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
---
+++ client/src/types/UpdateGameRequest.ts
@@ -0,0 +1,3 @@
+export interface UpdateGameRequest {
+  name: string
+}
